76bb245fca2d2bf5646616a98ae7da19de863a80
rafibu <rafaelburkhalter@live.com>
2021-12-07 20:42:02 +0100

    add author tag

--- src/main/java/ch/rafi/weather_oracle/WeatherOracleApplication.java
+++ src/main/java/ch/rafi/weather_oracle/WeatherOracleApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * @author rbu
+ */
 @SpringBootApplication
 public class WeatherOracleApplication {
 
